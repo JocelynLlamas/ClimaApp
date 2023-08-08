ad8ccaa245d588099bc4321b28fd2306ba4ca3eb
Jocelyn Llamas <mjlldlt@hotmail.com>
2023-08-07 19:49:03 -0600

    Visual Switch

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Switch, Col, Row } from 'antd';
+import './content.css'
 
 const Content = () => {
+
+    // F = True and C = False
+    const [active, setActive] = useState(true);
+    
+    const onChange = (checked) => {
+        console.log(`switch to ${checked}`);
+        setActive(checked)
+    };
+
     return (
-        <>
-            <div>Content</div>
-            <h1>HP;AAAA</h1>
-        </>
+        <div className='content'>
+            <Row justify="start" style={{marginLeft:'4%', marginTop:'1%'}}>
+                <Col span={1} className='degreesText'>
+                    <h1>°C</h1>
+                </Col>
+                <Col span={2} className='switch'>
+                    <Switch defaultChecked onChange={onChange} style={{backgroundColor: active ? '#237804' : '#73d13d'}}/>
+                </Col>
+                <Col span={1} className='degreesText'>
+                    <h1>°F</h1>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col span={24}>col</Col>
+            </Row>
+
+        </div>
     )
 }
 
